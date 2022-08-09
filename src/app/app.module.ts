417b07179cc10af4a4ce9ff202ed0d2113a86760
Anush Adhikari <aadhikari@ceiamerica.com>
2022-08-09 09:44:28 -0400

    push to github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatFoormComponent } from './chat-foorm/chat-foorm.component';
+import { ChatFormComponent } from './chat-form/chat-form.component';
+import { ChatRoomComponent } from './chat-room/chat-room.component';
+import { FeedComponent } from './feed/feed.component';
+import { MessageComponent } from './message/message.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserItemComponent } from './user-item/user-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatFoormComponent,
+    ChatFormComponent,
+    ChatRoomComponent,
+    FeedComponent,
+    MessageComponent,
+    LoginFormComponent,
+    SignupFormComponent,
+    NavbarComponent,
+    UserListComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule,
