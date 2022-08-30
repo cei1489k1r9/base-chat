43c173728b87cf96fdf5f11e9972eb49e5d0aa3a
Anush Adhikari <aadhikari@ceiamerica.com>
2022-08-30 09:46:41 -0400

    unit testing

---
+++ src/app/first-spec.ts
@@ -0,0 +1,23 @@
+describe('my first test', () => {
+let sut;
+
+beforeEach(()=>{
+    sut = {}
+})
+
+
+it('shoud be true if true', () => {
+
+
+//arrange
+sut.a = false; 
+
+//act
+sut.a = true;
+
+
+//assert
+expect(sut.a).toBe(true);
+   })
+})
+
